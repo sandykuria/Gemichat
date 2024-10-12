cc7da4bf5b2743e0eadd2ace5712dd7c457b7fba
Sandra <kuriasandra651@gmail.com>
2024-10-12 16:23:04 +0300

    Store chat history in a list

--- app/src/main/java/com/example/gemichat/ChatViewModel.kt
+++ app/src/main/java/com/example/gemichat/ChatViewModel.kt
@@ -1,12 +1,18 @@
 package com.example.gemichat
 
 import android.util.Log
+import android.view.textclassifier.ConversationActions.Message
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.ai.client.generativeai.GenerativeModel
 import kotlinx.coroutines.launch
 
 class ChatViewModel: ViewModel() {
+
+    val messageList by lazy {
+        mutableStateListOf<MessageModel>()
+    }
 
     val generativeModel : GenerativeModel = GenerativeModel(
         modelName = "gemini-pro",
@@ -16,7 +22,11 @@ class ChatViewModel: ViewModel() {
     fun sendMessage(question : String){
         viewModelScope.launch {
             val chat = generativeModel.startChat()
+
+            messageList.add(MessageModel(question, "User"))
+
             val response = chat.sendMessage(question)
+            messageList.add(MessageModel(response.text.toString(), "model"))
             Log.i("Response from Gemini", response.text.toString())
         }
     }
