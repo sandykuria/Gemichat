ebc56d946409c4dc5078806d15c8c1ddbf567b4b
Sandra <kuriasandra651@gmail.com>
2024-10-12 15:26:56 +0300

    Text input UI

--- app/src/main/java/com/example/gemichat/ChatPage.kt
+++ app/src/main/java/com/example/gemichat/ChatPage.kt
@@ -1,13 +1,25 @@
 package com.example.gemichat
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -19,6 +31,33 @@ fun ChatPage (modifier: Modifier = Modifier){
         modifier = modifier
     ){
         AppHeader()
+        MessageInput(onMesssageSend = {
+
+        })
+    }
+}
+@Composable
+fun MessageInput(onMesssageSend: (String)-> Unit){
+    var message by remember {
+        mutableStateOf("")
+    }
+    Row (
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        OutlinedTextField(
+            modifier = Modifier.weight(1f),
+            value = message,
+            onValueChange = {
+                message = it
+            }
+        )
+        IconButton(onClick = {
+            onMesssageSend(message)
+            message = ""
+        }) {
+            Icon(imageVector = Icons.Default.Send, contentDescription = "Send")
+        }
     }
 }
 
@@ -35,3 +74,4 @@ fun AppHeader(){
             )
     }
 }
+
