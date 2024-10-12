d358747360a569ad2c43f35817341e69e574000b
Sandra <kuriasandra651@gmail.com>
2024-10-12 17:34:12 +0300

    chat message and chat list UI

--- app/src/main/java/com/example/gemichat/ChatPage.kt
+++ app/src/main/java/com/example/gemichat/ChatPage.kt
@@ -6,6 +6,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
@@ -20,9 +23,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.gemichat.ui.theme.ColorModelMessage
+import com.example.gemichat.ui.theme.ColorUserMessage
 
 @Composable
 fun ChatPage (modifier: Modifier = Modifier,viewModel: ChatViewModel){
@@ -30,11 +37,58 @@ fun ChatPage (modifier: Modifier = Modifier,viewModel: ChatViewModel){
         modifier = modifier
     ){
         AppHeader()
+        MessageList(modifier = Modifier.weight(1f), messageList = viewModel.messageList)
         MessageInput(onMesssageSend = {
             viewModel.sendMessage(it)
-        })
+            }
+        )
     }
 }
+
+@Composable
+fun MessageList(modifier: Modifier = Modifier,messageList : List<MessageModel>){
+    LazyColumn(
+        modifier = Modifier,
+        reverseLayout = true
+    ) {
+        items(messageList.reversed()){
+            MessageRow(messageModel = it)
+        }
+    }
+}
+
+@Composable
+fun MessageRow(messageModel: MessageModel){
+    val isModel = messageModel.role=="model"
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Box(
+                modifier = Modifier.align(if (isModel) Alignment.BottomStart else Alignment.BottomEnd)
+                    .padding(
+                        start = if(isModel) 8.dp else 70.dp,
+                        end = if (isModel) 70.dp else 8.dp,
+                        top = 8.dp,
+                        bottom = 8.dp
+                    )
+                    .clip(RoundedCornerShape(48f))
+                    .background(if (isModel) ColorModelMessage else ColorUserMessage)
+                    .padding(16.dp)
+            ){
+                Text(
+                    text = messageModel.message,
+                    fontWeight = FontWeight.W500,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
 @Composable
 fun MessageInput(onMesssageSend: (String)-> Unit){
     var message by remember {
