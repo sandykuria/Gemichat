96ca1a77e67caca87aff369772d048a241ac5c2d
Sandra <kuriasandra651@gmail.com>
2024-10-12 14:46:30 +0300

    App Header

--- app/src/main/java/com/example/gemichat/ChatPage.kt
+++ app/src/main/java/com/example/gemichat/ChatPage.kt
@@ -1,9 +1,37 @@
 package com.example.gemichat
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ChatPage (modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier
+    ){
+        AppHeader()
+    }
+}
 
+@Composable
+fun AppHeader(){
+    Box (
+        modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.primary)
+    ){
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = "Gemichat",
+            color = Color.White,
+            fontSize = 22.sp
+            )
+    }
 }
